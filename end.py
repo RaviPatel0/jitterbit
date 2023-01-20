6fdbaaf94021be13a0730c66c189236727618a69
RaviPatel0 <ravinandasana1998@gmail.com>
2023-01-20 13:00:08 +0530

    Add

---
+++ end.py
@@ -0,0 +1,11 @@
+file = open("end.txt","w")
+L = ["This is Python \n","This is after deploy \n"]
+
+# i assigned ["This is Lagos \n","This is Python \n","This is Fcc \n"] to #variable L, you can use any letter or word of your choice.
+# Variable are containers in which values can be stored.
+# The \n is placed to indicate the end of the line.
+
+file.write("Hello There \n")
+file.writelines(L)
+file.close()
+
